752db039d3239a8a93c37d067af1031e276cd982
Marius <marius@MacBook-Air-Marius.local>
2024-10-01 22:04:18 +0300

    two summ

---
+++ easy lvl/two sum.py
@@ -0,0 +1,9 @@
+def two_sum(num, target):
+    for i in range(len(num)):
+        for j in range(i + 1, len(num)):
+            if num[i] + num[j] == target:
+                return [i, j]
+
+num = [1,2,3,4,5,6,7,8]
+target = 9
+print(two_sum(num, target))
